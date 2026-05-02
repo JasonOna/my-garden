0d817189b680437a85b8aa3b34f2882e06857f43
Jason Ona <onajjb@gmail.com>
2026-05-02 20:41:09 +1000

    Quartz sync: May 2, 2026, 8:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jasonona.github.io/my-garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
